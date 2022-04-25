9322a188376a80d54851b41f9bb3b0fd4a14a13e
BerezovskyiRuslan <berezovskyiruslanwork@gmail.com>
2022-04-25 11:39:30 +0300

    Create note with use react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Routers from './router/Router';
+import './assets/styles/styles.css';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routers />
+    </div>
+  );
+}
+
+export default App;
